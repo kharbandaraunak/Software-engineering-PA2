b719e667ff2166c8a46e5a524c72ae70046e421a
Raunak Kharbanda <raunak@tamu.edu>
2024-09-19 00:58:28 -0500

    3 movies added

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,17 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Sherlock', :rating => 'PG-13',
+    :release_date => '15-Jun-2017'},
+  {:title => 'Inception', :rating => 'PG-13',
+    :release_date => '02-Jan-2020'},
+  {:title => '2012', :rating => 'PG-13',
+    :release_date => '12-April-2012'}
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  unless Movie.exists?(title: movie[:title])
+    Movie.create!(movie)
+  end
 end
